490db0b213dcb0de26b53bc1723d852a7a233fe9
Santiago Botero Peláez <botero.pelaez.santiago@gmail.com>
2021-01-13 17:22:05 -0500

    [EDIT] Product description

--- backend/store.py
+++ backend/store.py
@@ -2,42 +2,65 @@ products = [
     {
         'code': 1,
         "name": 'samsung',
+        'description': '''cpu: 2GHz
+        ram: 2Gb
+        camera: 13mp''',
         'price': 600000,
         'stock': 200
     },
     {
         'code': 2,
         "name": 'lg',
+        'description': '''cpu: 1GHz
+        ram: 4Gb
+        camera: 8mp''',
         'price': 400000,
         'stock': 50
     },
     {
         'code': 3,
         "name": 'nokia',
+        'description': '''cpu: 20GHz
+        ram: 24Gb
+        camera: 64mp''',
         'price': 100000,
         'stock': 20
     },
     {
         'code': 4,
         "name": 'realme',
+        'description': '''cpu: 2GHz
+        ram: 4Gb
+        camera: 64mp''',
         'price': 700000,
         'stock': 5
     },
     {
         'code': 5,
         "name": 'xiaomi',
+        'description': '''cpu: 2GHz
+        ram: 8Gb
+        camera: 48mp''',
         'price': 1000000,
         'stock': 150
     },
     {
         'code': 6,
         "name": 'huawei',
+        'description': '''cpu: 1GHz
+        ram: 4Gb
+        camera: 13mp''',
         'price': 600000,
         'stock': 5
     },
     {
         'code': 7,
         "name": 'iphone',
+        'description': '''cpu: 1GHz
+        ram: 1Gb
+        camera: 2mp
+        
+        A very expensive and useless cellphone''',
         'price': 6000000,
         'stock': 2000
     },
